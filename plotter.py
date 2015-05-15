4978b2bd83b30896dbbce2d604f879053f970fd0
cowdew <Jespr@Jespers-Air.lan>
2015-05-15 17:10:05 +0200

    Conveyer class introduced.
    Readme.

---
+++ plotter.py
@@ -0,0 +1,59 @@
+import sys
+import nxt
+import usb
+import time
+
+class Conveyer:
+
+    def __init__(self, device):
+
+        self.port  = nxt.motor.PORT_A
+        self.thrust, self.tacho = 100, 90
+
+        self.motor = nxt.motor.Motor(
+            device, self.port
+        )
+
+    def _motor_on(self):
+
+        motor_state = self.motor._read_state()
+        motor_state = state[0].mode
+
+        return (motor_state == nxt.motor.MODE_MOTOR_ON)
+
+    def _motor_wait(self):
+
+        # Wait for motor mode to change
+        while self._motor_on():
+            continue
+
+    def left(self):
+
+        self.motor.weak_turn(-self.thrust, self.tacho)
+        self._motor_wait()
+        self.motor.idle()
+
+    def right(self):
+
+        self.motor.weak_turn(self.thrust, self.tacho)
+        self._motor_wait()
+        self.motor.idle()
+
+def main():
+
+    try:
+        sock = nxt.locator.find_one_brick()
+    except nxt.locator.BrickNotFoundError:
+        print 'Brick not found'
+        sys.exit(1)
+
+    
+    conveyer = Conveyer(sock)
+
+    conveyer.left()
+    conveyer.right()
+
+    time.sleep(1)
+
+if __name__ == '__main__':
+    main()
